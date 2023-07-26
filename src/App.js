a4b6c77281b23c65a705b4cc84c1bf52f8e91b38
Milly Prashar <millyprashar@gmail.com>
2023-07-26 14:58:04 -0700

    Added the profile section of the left sidebar + stylings

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Sidebar from './features/Sidebar';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       {/* Header */}
       < Header />
       {/* App Body */}
+      <div className="app__body">
+        <Sidebar />
+      </div>
         {/* Sidebar */}
         {/* Feed */}
         {/* Widgets */}
---
+++ src/features/Sidebar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./Sidebar.css"
+import { Avatar } from '@mui/material'
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="sidebar__top">
+            <img src="https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/image/rDtN98Qoishumwih/summer-background-abstract-background-wallpaper-use-for-presentation_HDDYoJOhMg_thumb.jpg" alt="" />
+            <Avatar className="sidebar__avatar" />
+            <h2>Milly Prashar</h2>
+            <h4>millyprashar@gmail.com</h4>
+        </div>
+        <div className="sidebar__stats">
+            <div className="sidebar__stat">
+                <p>Who viewed you</p>
+                <p className="sidebar__statNumber">2,543</p>
+            </div>
+            <div className="sidebar__stat">
+                <p>Who viewed you</p>
+                <p className="sidebar__statNumber">2,448</p>
+            </div>
+        </div>
+        <div className="sidebar__bottom">
+            <p>Recent</p>
+        </div>
+    </div>
+  )
+}
+
+export default Sidebar
